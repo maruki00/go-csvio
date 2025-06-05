3033947de6ded9c16656f03de0d933dae02c4feb
maruki00 <aitbaha48@gmail.com>
2025-06-05 17:34:12 +0100

    commit

--- csv_reader.go
+++ csv_reader.go
@@ -1,14 +1,75 @@
 package gocsvio
 
-import "os"
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"os"
+)
+
+var (
+	ErrOpenFile            = errors.New("could not open the csv file")
+	ErrFileNotAccessible   = errors.New("could not access this csv file")
+	ErrFileProbablyEmpty   = errors.New("file probably empty")
+	ErrCouldNotReadTheFile = errors.New("could not read the file")
+)
 
 type CSV struct {
-	_headers map[string]int
-	line     []string
-	fd       *os.File
+	_headers  map[string]int
+	line      []string
+	seperator []byte
+	fd        *os.File
 }
 
-func NewReader()
+func NewReader(csvpath string) (*CSV, error) {
+	fd, err := os.OpenFile(csvpath, os.O_CREATE|os.O_RDWR, os.FileMode.Perm())
+	if err != nil {
+		return nil, ErrOpenFile
+	}
+	obj := &CSV{
+		fd: fd,
+	}
+	if err := obj.parseHeader(); err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+func (_this *CSV) yield() (<-chan string, error) {
+	scanner := bufio.NewScanner(_this.fd)
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	chnl := make(chan string)
+	go func() {
+		for scanner.Scan() {
+			chnl <- scanner.Text()
+		}
+		close(chnl)
+	}()
+	return chnl, nil
+}
+
+func (_this *CSV) parseHeader() error {
+	if _this.fd == nil {
+		return ErrFileNotAccessible
+	}
+	reader := bufio.NewReader(_this.fd)
+	_, err := _this.fd.Seek(int64(0), 0)
+	if err != nil {
+		return ErrFileProbablyEmpty
+	}
+	header, _, err := reader.ReadLine()
+	if err != nil {
+		return ErrCouldNotReadTheFile
+	}
+	headers := bytes.Split(header, _this.seperator)
+	for i, header := range headers {
+		_this._headers[string(header)] = i
+	}
+	return nil
+}
+
 func (_this *CSV) Get(key string) any {
 	index, ok := _this._headers[key]
 	if !ok {
